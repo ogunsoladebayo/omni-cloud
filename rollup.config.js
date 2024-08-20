b2c5df8e8e86523662a3bd289c624e067e8e309d
ogunsoladebayo <usman.ogunsola@outlook.com>
2024-08-20 23:32:37 +0100

    feat: added rollup and updated tsconfig.json

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+const typescript = require("rollup-plugin-typescript2")
+const pkg = require("./package.json")
+
+module.exports = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false
+    }
+  ],
+  plugins: [
+    typescript()
+  ],
+}
