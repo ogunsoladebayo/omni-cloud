4ddbcd224bf908f0b2c60441df2e88f8eac39f41
Vikram Singh <nagwalanshu@gmail.com>
2024-09-23 10:27:25 +0530

    feat: Updated Error Handling utility and Providers

--- src/interfaces/IProviderConfig.ts
+++ src/interfaces/IProviderConfig.ts
@@ -8,3 +8,5 @@ export interface IProviderConfig {
   google?: IGoogleConfig;
   azure?: IAzureConfig;
 }
+
+export type Provider = IProviderConfig['provider'];
--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -1,25 +1,30 @@
+import { Provider } from "../interfaces/IProviderConfig"
+
 interface IError {
     statusCode: number;
     error: string[];
     data: any;
     success: boolean;
-    provider: string;
+    provider: Provider;
+    errorType: string;
 }
 
-class ErrorHandler extends Error implements IError {
+export class ErrorHandler extends Error implements IError {
     public statusCode: number;
     public error: string[];
     public data: any;
     public success: boolean;
-    public provider: string;
+    public provider: Provider;
+    public errorType: string;
 
-    constructor( provider: string, message: string = "Something went wrong", statusCode: number = 500, error: string[] = [], stack?: string) {
+    constructor( provider: Provider, errorType: string, message: string = "Something went wrong", statusCode: number = 500, error: string[] = [], stack?: string) {
         super(message);
         this.statusCode = statusCode;
         this.error = error;
         this.data = null;
         this.success = false;
         this.provider = provider
+        this.errorType = errorType;
 
         if (stack) {
             this.stack = stack;
@@ -28,5 +33,3 @@ class ErrorHandler extends Error implements IError {
         }
     }
 }
-
-export { ErrorHandler }
